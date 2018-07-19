c03fdaad569adff5f41b42b9973bee4cb775c53d
Constantine Jin <constantine.jin@blackboard.com>
2018-07-19 10:05:33 +0800

    add

---
+++ step1.groovy
@@ -0,0 +1,40 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'qopper pr1 Building..'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'qopper pr1 Testing..'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'qopper pr1 Deploying....'
+            }
+        }
+        stage('Next Job') {
+            steps {
+                build job: 'test_multibranch2/master',
+                        parameters: [
+                                [
+                                        $class: 'BooleanParameterValue',
+                                        name: 'someBooleanParameter',
+                                        value: true,
+                                ],
+                                [
+                                        $class: 'StringParameterValue',
+                                        name: 'someStringParameter',
+                                        value: 'some string value',
+                                ]
+                        ],
+                        propagate: false
+
+            }
+        }
+    }
+}
