19eaee36ce5275272e2cf9435b14770c8c54df74
Constantine Jin <constantine.jin@blackboard.com>
2018-07-20 11:37:33 +0800

    add

--- src/org/step1.groovy
+++ src/org/step1.groovy
@@ -19,7 +19,23 @@ pipeline {
                             script{
                                 def pipeline_json=[["stage":"Next Job 1","index":1],["stage":"Next Job 2","index":2]]
 //                                def jsonOut = readJSON text: groovy.json.JsonOutput.toJson(pipeline_json)
-                                writeJSON(file: '/tmp/message1.json', json: pipeline_json)
+                                def data = [
+                                        attachments:[
+                                                [
+                                                        fallback: "New open task [Urgent]: <http://url_to_task|Test out Slack message attachments>",
+                                                        pretext : "New open task [Urgent]: <http://url_to_task|Test out Slack message attachments>",
+                                                        color   : "#D00000",
+                                                        fields  :[
+                                                                [
+                                                                        title: "Notes",
+                                                                        value: "This is much easier than I thought it would be.",
+                                                                        short: false
+                                                                ]
+                                                        ]
+                                                ]
+                                        ]
+                                ]
+                                writeJSON(file: '/tmp/message1.json', json: data)
                             }
                         }
                     }
