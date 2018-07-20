6433d5edacb15283b811608b115c2bdd01a1a8b8
Constantine Jin <constantine.jin@blackboard.com>
2018-07-20 11:56:10 +0800

    add

--- src/org/step1.groovy
+++ src/org/step1.groovy
@@ -17,25 +17,10 @@ pipeline {
                     steps{
                         script{
                             script{
-                                def pipeline_json=[["stage":"Next Job 1","index":1],["stage":"Next Job 2","index":2]]
+//                                def pipeline_json=[["stage":"Next Job 1","index":1],["stage":"Next Job 2","index":2]]
+                                def pipeline_json=readJSON file: 'myfile.json'
 //                                def jsonOut = readJSON text: groovy.json.JsonOutput.toJson(pipeline_json)
-                                def data = [
-                                        attachments:[
-                                                [
-                                                        fallback: "New open task [Urgent]: <http://url_to_task|Test out Slack message attachments>",
-                                                        pretext : "New open task [Urgent]: <http://url_to_task|Test out Slack message attachments>",
-                                                        color   : "#D00000",
-                                                        fields  :[
-                                                                [
-                                                                        title: "Notes",
-                                                                        value: "This is much easier than I thought it would be.",
-                                                                        short: false
-                                                                ]
-                                                        ]
-                                                ]
-                                        ]
-                                ]
-                                writeJSON(file: 'message1.json', json: data)
+                                writeJSON(file: 'message1.json', json: pipeline_json)
                             }
                         }
                     }
