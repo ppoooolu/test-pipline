54a723bead7d50ac172e7ce95204de275964f1d5
Constantine Jin <constantine.jin@blackboard.com>
2018-10-12 11:21:06 +0800

    add

--- flows/test_mulit_jobs.groovy
+++ flows/test_mulit_jobs.groovy
@@ -4,7 +4,7 @@ def container_Template = libraryResource 'com/k8s/containerTemplate.yaml'
 pipeline {
     parameters {
         string(name: 'job_id', defaultValue: 'xxxxxxx', description: 'job id')
-        multiline(name: 'other_parameters', defaultValue: '', description: 'other parameters')
+        text(name: 'other_parameters', defaultValue: '', description: 'other parameters')
     }
 
     stages {
