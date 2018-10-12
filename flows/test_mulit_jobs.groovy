a5b1f760b1566b051526706ecf22cb36a73c99fb
Constantine Jin <constantine.jin@blackboard.com>
2018-10-12 11:26:33 +0800

    add

--- flows/test_mulit_jobs.groovy
+++ flows/test_mulit_jobs.groovy
@@ -3,7 +3,7 @@ def container_Template = libraryResource 'com/k8s/containerTemplate.yaml'
 
 pipeline {
     agent any
-    
+
     parameters {
         string(name: 'job_id', defaultValue: 'xxxxxxx', description: 'job id')
         text(name: 'other_parameters', defaultValue: '', description: 'other parameters')
@@ -14,6 +14,11 @@ pipeline {
             steps {
                 script {
                     echo "${params.other_parameters}"
+
+                    def xx = ${params.other_parameters}.split('\n')
+
+                    echo xx
+
 //                    def _result = build job: 'test_step_1/master',
 //                            parameters: [
 //                                    [
