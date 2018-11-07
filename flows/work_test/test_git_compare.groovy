e85e01a4383c423d99e830d16e207b22ce407e39
Constantine Jin <constantine.jin@blackboard.com>
2018-11-07 16:29:41 +0800

    add

--- flows/work_test/test_git_compare.groovy
+++ flows/work_test/test_git_compare.groovy
@@ -1,13 +1,26 @@
 
+def check_status = true
 pipeline {
     agent any
     triggers { pollSCM('* * * * *') }
     stages {
-        stage('Get Repo') {
-            steps {
-                dir('${env.BRANCH_NAME}'){
-                    echo 'aaaaaabbbb'
-//                    git branch: BRANCH_NAME, url: 'git@bitbucket.org:user/test1.git', credentialsId: 'credentials_id'
+        stage('Check branch'){
+            steps{
+                if (env.BRANCH_NAME=='master'){
+                    check_status =false
+                }
+            }
+        }
+        if (check_status) {
+            stage('Get Repo') {
+                steps {
+                    dir(current_version) {
+                        git branch: BRANCH_NAME, url: 'git@bitbucket.org:user/test1.git', credentialsId: 'credentials_id'
+                    }
+
+                    dir('${env.BRANCH_NAME}') {
+                        git branch: BRANCH_NAME, url: 'git@bitbucket.org:user/test1.git', credentialsId: 'credentials_id'
+                    }
                 }
             }
         }
