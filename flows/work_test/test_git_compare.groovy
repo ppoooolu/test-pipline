1a6dcc18c636f7547a2ed9823bdf3eea2212d4ba
Constantine Jin <constantine.jin@blackboard.com>
2018-11-07 15:07:57 +0800

    add

---
+++ flows/work_test/test_git_compare.groovy
@@ -0,0 +1,14 @@
+
+pipeline {
+    agent any
+
+    stages {
+        stage('Get Repo') {
+            steps {
+                dir('${env.BRANCH_NAME}'){
+                    echo 'aaaaaa'
+                }
+            }
+        }
+    }
+}
