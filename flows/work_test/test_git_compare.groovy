47188b2b830b6a88313e4ad4e794c4076725f0ea
Constantine Jin <constantine.jin@blackboard.com>
2018-11-07 15:44:35 +0800

    add

--- flows/work_test/test_git_compare.groovy
+++ flows/work_test/test_git_compare.groovy
@@ -1,12 +1,13 @@
 
 pipeline {
     agent any
-
+    triggers { pollSCM('* * * * *') }
     stages {
         stage('Get Repo') {
             steps {
                 dir('${env.BRANCH_NAME}'){
                     echo 'aaaaaabbbb'
+//                    git branch: BRANCH_NAME, url: 'git@bitbucket.org:user/test1.git', credentialsId: 'credentials_id'
                 }
             }
         }
