ba2bc8b1011a38acfc8403a82dba71ab11c4f320
Constantine Jin <constantine.jin@blackboard.com>
2018-11-23 18:00:22 +0800

    add

--- vars/pipeline_common_stage.groovy
+++ vars/pipeline_common_stage.groovy
@@ -1,3 +1,13 @@
+def check_status(file,key1,key2){
+    if (!fileExists(file)) {return false}
+    def check_file_json = readJSON file: file
+    echo "${check_file_json[key1][key2]}"
+    if (check_file_json[key1][key2] == 'SUCCESS'){
+        return true
+    }
+    return false
+}
+
 def common_stage(stage_name,stage_values){
     stage(stage_name) {
 //        steps {
