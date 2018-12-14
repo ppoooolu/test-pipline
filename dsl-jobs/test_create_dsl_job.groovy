010e35cfb1c77a9265a52c80718ee551d59fc980
Constantine Jin <constantine.jin@blackboard.com>
2018-12-14 11:08:52 +0800

    add

--- dsl-jobs/test_create_dsl_job.groovy
+++ dsl-jobs/test_create_dsl_job.groovy
@@ -155,96 +155,21 @@ jobList.each{job_i->
     String stash_credentialsId ='4af2df8-e947-4ae0-ba51-b2a25690304d'
     String includes_branch = job_i.includes_branch
     String excludes_branch = job_i.excludes_branch
-    
-    MultibranchWorkflowJob build(DslFactory dslFactory) {
-        MultibranchWorkflowJob job = dslFactory.multibranchPipelineJob(name) {
-            it.description this.description
-        }
-        job.with {
-            branchSources {
-                git {
-                    remote('ssh://git@stash.bbpd.io/'+stashProject+'/'+stashRepo)
-                    credentialsId(stash_credentialsId)
-                    includes(includes_branch)
-                    excludes(excludes_branch)
-//                    traits {
-//                        submoduleOptionTrait {
-//                            extension {
-//                                disableSubmodules(false)
-//                                recursiveSubmodules(true)
-//                                trackingSubmodules(false)
-//                                reference(null)
-//                                timeout(null)
-//                                parentCredentials(true)
-//                            }
-//                        }
-//
-//                        cloneOptionTrait {
-//                            extension {
-//                                shallow(false)
-//                                noTags(false)
-//                                reference(null)
-//                                depth(0)
-//                                honorRefspec(false)
-//                                timeout(10)
-//                            }
-//                        }
-//                    }
-                }
 
-            }
-
-            configure {
-                it / factory(class: 'org.jenkinsci.plugins.workflow.multibranch.WorkflowBranchProjectFactory') {
-                    owner(class: 'org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject', reference: '../..')
-                    scriptPath(jenkinsfile_path)
-                }
-
-                it / sources / 'data' / 'jenkins.branch.BranchSource' << {
-//                    source(class: 'jenkins.plugins.git.GitSCMSource') {
-//                        id(uuid)
-//                        remote("git@gitlab:root/repo.git")
-//                        credentialsId("ssh_key")
-//                        includes('*')
-//                        excludes('')
-//                        ignoreOnPushNotifications('false')
-//                        traits {
-//                            'jenkins.plugins.git.traits.BranchDiscoveryTrait'()
-//                        }
-//                    }
-
-                    // default strategy when sourcing from a branch
-//                    strategy(class: "jenkins.branch.NamedExceptionsBranchPropertyStrategy") {
-//                        defaultProperties(class: "java.util.Arrays\$ArrayList") {
-//                            a(class: "jenkins.branch.BranchProperty-array") {
-//                                // don't trigger builds
-//                                "jenkins.branch.NoTriggerBranchProperty"()
-//                            }
-//                        }
-//                    }
-
-//                    strategy(class: "jenkins.branch.DefaultBranchPropertyStrategy") {
-//                        defaultProperties(class: "java.util.Arrays\$ArrayList") {
-//                            a(class: "jenkins.branch.BranchProperty-array") {
-//                                // don't trigger builds
-//                                "jenkins.branch.NoTriggerBranchProperty"()
-//                            }
-//                        }
-//                    }
-
-                }
+    multibranchPipelineJob('example') {
+        branchSources {
+            git {
+                remote('ssh://git@stash.bbpd.io/'+stashProject+'/'+stashRepo)
+                credentialsId(stash_credentialsId)
+                includes(includes_branch)
+                excludes(excludes_branch)
             }
         }
-
+        orphanedItemStrategy {
+            discardOldItems {
+                numToKeep(20)
+            }
+        }
     }
-//    Job collectParamsJob = new CreateMulitibranchPipelineJob(
-//            name: job_i.name,
-//            description: job_i.description,
-//            stashProject: job_i.stashProject,
-//            stashRepo: job_i.stashRepo,
-//            excludes_branch: job_i.excludes_branch,
-//            includes_branch: job_i.includes_branch,
-//            jenkinsfile_path: job_i.jenkinsfile_path
-//    ).build(this)
 }
 
