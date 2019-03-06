4f861411d43868ee4693a56eff3fc48220665871
Constantine Jin <constantine.jin@blackboard.com>
2019-03-06 13:43:37 +0800

    add

--- src/org/SuspendJob.groovy
+++ src/org/SuspendJob.groovy
@@ -4,6 +4,12 @@ import jenkins.branch.*
 import jenkins.model.Jenkins
 
 class SuspendJob {
+
+    static final BBops_Suspend_Job_list = [
+            'SaaS/test_env',
+            'SaaS/test_env2'
+    ]
+
     static void suspend(){
         for (f in Jenkins.instance.getAllItems(jenkins.branch.MultiBranchProject.class)) {
             if (f.parent instanceof jenkins.branch.OrganizationFolder) {
@@ -22,19 +28,21 @@ class SuspendJob {
     }
 
     static void suspend2() {
-//        for (f in Jenkins.instance.getItemByFullName('SaaS/test_env')){
-        for (f in    Jenkins.instance.getItemMap()['test_env','test_env2']){
+        for (job_name in BBops_Suspend_Job_list) {
+            for (f in Jenkins.instance.getItemByFullName(job_name)) {
+//        for (f in  Jenkins.instance.getItemMap()['test_env','test_env2']){
 //            if (f.parent instanceof jenkins.branch.OrganizationFolder) {
 //                continue;
 //            }
-            for (s in f.sources) {
-                def prop = new jenkins.branch.NoTriggerBranchProperty();
-                def propList = [prop] as jenkins.branch.BranchProperty[];
-                def strategy = new jenkins.branch.DefaultBranchPropertyStrategy(propList);
-                s.setStrategy(strategy);
-            }
+                for (s in f.sources) {
+                    def prop = new jenkins.branch.NoTriggerBranchProperty();
+                    def propList = [prop] as jenkins.branch.BranchProperty[];
+                    def strategy = new jenkins.branch.DefaultBranchPropertyStrategy(propList);
+                    s.setStrategy(strategy);
+                }
 
-            f.computation.run()
+                f.computation.run()
+            }
         }
     }
 }
