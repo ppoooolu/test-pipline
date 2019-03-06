8e872aa4d7e55f010a32ea3b78ed1011dacaa22a
Constantine Jin <constantine.jin@blackboard.com>
2019-03-06 11:05:17 +0800

    fix

--- dsl-jobs/test_create_dsl_job_new.groovy
+++ dsl-jobs/test_create_dsl_job_new.groovy
@@ -31,64 +31,73 @@ jobList = [
 //        ]
 ]
 
-jobList.each{job_i->
+jobList.each { job_i ->
     String name = job_i.name
-    String job_description =job_i.description
+    String job_description = job_i.description
     String stashProject = job_i.stashProject
     String stashRepo = job_i.stashRepo
     String stashBranch = 'master'
     String jenkinsfile_path = job_i.jenkinsfile_path
-    String stash_credentialsId ='stash'
+    String stash_credentialsId = 'stash'
     String includes_branch = job_i.includes_branch
     String excludes_branch = job_i.excludes_branch
     String job_folder = job_i.folder
 
-    multibranchPipelineJob(job_folder+'/'+name) {
+    multibranchPipelineJob(job_folder + '/' + name) {
         description(job_description)
         branchSources {
             branchSource {
+//                source {
+//                    git {
+//                        remote('https://github.com/' + stashProject + '/' + stashRepo)
+//                        credentialsId(stash_credentialsId)
+//                        includes(includes_branch)
+//                        excludes(excludes_branch)
+//                    }
+//                }
 
                 source {
                     git {
-                        remote('git@github.com:ppoooolu/test-pipline.git')
-                        credentialsId( stash_credentialsId )
-                        traits {
-                            gitBrowserSCMSourceTrait {
-                                browser {
-                                    stash {
-                                        repoUrl( 'https://github.com/' + stashProject + '/' + stashRepo)
-                                    }
-                                }
+                        remote('https://github.com/' + stashProject + '/' + stashRepo)
+                        credentialsId(stash_credentialsId)
+//                        traits {
+//                            gitBrowserSCMSourceTrait {
+//                                browser {
+//                                    stash {
+//                                        repoUrl( 'https://github.com/' + stashProject + '/' + stashRepo)
+//                                    }
+//                                }
+//                            }
+//                            pruneStaleBranchTrait()
+//                            branchDiscoveryTrait()
+//                            ignoreOnPushNotificationTrait()
+//                        }
+//                    }
+                    }
+
+                    strategy {
+                        defaultBranchPropertyStrategy {
+                            props {
+                                noTriggerBranchProperty()
                             }
-                            pruneStaleBranchTrait()
-                            branchDiscoveryTrait()
-                            ignoreOnPushNotificationTrait()
                         }
                     }
+
                 }
+            }
 
-                strategy {
-                    defaultBranchPropertyStrategy {
-                        props {
-                            noTriggerBranchProperty()
-                        }
-                    }
+            factory {
+                workflowBranchProjectFactory {
+                    scriptPath(jenkinsfile_path)
                 }
-
             }
-        }
 
-        factory {
-            workflowBranchProjectFactory {
-                scriptPath(jenkinsfile_path)
-            }
-        }
-
-        orphanedItemStrategy {
-            discardOldItems {
-                numToKeep(200)
+            orphanedItemStrategy {
+                discardOldItems {
+                    numToKeep(200)
+                }
             }
         }
     }
-}
 
+}
