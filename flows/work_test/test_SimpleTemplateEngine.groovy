f52afa6ac87cf40575b8db4d87b41191bea2d930
Constantine Jin <constantine.jin@blackboard.com>
2019-06-25 15:15:21 +0800

    add

--- flows/work_test/test_SimpleTemplateEngine.groovy
+++ flows/work_test/test_SimpleTemplateEngine.groovy
@@ -1,28 +1,35 @@
 @Library('my-lib') pipelineLibrary
 import groovy.text.SimpleTemplateEngine
 
+@NonCPS
+def renderTemplete(){
+    Map<String, String> newrelicOption = [:]
+    newrelicOption.put('aaaa', '1111')
+    newrelicOption.put('bbbb', '2222')
+    echo 'aaaaa'
+    def DISABLE_NEW_RELIC_SYNTHETIC_SCRIPT='''#!/bin/bash
+set -e
+echo ********
+TEST_ALERT_POLICIES="Test - Synthetics Alerts"
+echo "${'$'}{TEST_ALERT_POLICIES}"
+echo <% newrelicOption.each { name, val -> print "${val}" } %>
+'''
+    def engine = new groovy.text.SimpleTemplateEngine()
+    def templateString = DISABLE_NEW_RELIC_SYNTHETIC_SCRIPT
+    echo 'bbbbbbb'
+    def ex_script = engine.createTemplate(templateString).make(['newrelicOption':newrelicOption])
+    echo 'ccccc'
+    return ex_script.toString()
+}
+
 pipeline {
     agent any
     stages {
         stage('test') {
             steps {
                 script {
-                    Map<String, String> newrelicOption = [:]
-                    newrelicOption.put('aaaa', '1111')
-                    newrelicOption.put('bbbb', '2222')
-                    def DISABLE_NEW_RELIC_SYNTHETIC_SCRIPT='''#!/bin/bash
-set -e
-echo ********
-newrelicOption.each{ name, val -> print "--${name} ${val} " }
-'''
-                    SimpleTemplateEngine engine = new SimpleTemplateEngine()
-                    String templateString = DISABLE_NEW_RELIC_SYNTHETIC_SCRIPT
-                    String script = engine.createTemplate(templateString).make(
-                            [
-                                'newrelicOption':newrelicOption,
-                            ]
-                    )
-                    sh script
+                    def run_script = renderTemplete()
+                    echo run_script
                 }
             }
         }
