238cf3537ba14d30028d2bbb3a71555e7c037371
Constantine Jin <constantine.jin@blackboard.com>
2019-06-25 14:32:23 +0800

    add

---
+++ flows/work_test/test_SimpleTemplateEngine.groovy
@@ -0,0 +1,29 @@
+@Library('my-lib') pipelineLibrary
+
+pipeline {
+    agent any
+    stages {
+        stage('test') {
+            steps {
+                script {
+                    Map<String, String> newrelicOption = [:]
+                    newrelicOption.put('aaaa', '1111')
+                    newrelicOption.put('bbbb', '2222')
+                    def DISABLE_NEW_RELIC_SYNTHETIC_SCRIPT='''#!/bin/bash
+set -e
+echo ********
+newrelicOption.each{ name, val -> print "--${name} ${val} " }
+'''
+                    SimpleTemplateEngine engine = new SimpleTemplateEngine()
+                    String templateString = DISABLE_NEW_RELIC_SYNTHETIC_SCRIPT
+                    String script = engine.createTemplate(templateString).make(
+                            [
+                                'newrelicOption':newrelicOption,
+                            ]
+                    )
+                    sh script
+                }
+            }
+        }
+    }
+}
--- flows/work_test/test_hooks.groovy
+++ flows/work_test/test_hooks.groovy
@@ -2,7 +2,7 @@
 
 pipeline {
     agent any
-
+    triggers { pollSCM('') }
     stages {
         stage('ls') {
             steps {
